aceca7141af39ed560dc073506792d73f32a8319
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-03-29 10:12:43 +0200

    Fix button links not working properly

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,15 +4,18 @@ import { useRouter } from '@/i18n/navigation'
 
 export type ButtonProps = AntdButtonProps
 
-export function Button({ onClick, href, ...props }: AntdButtonProps) {
+export function Button({ onClick, href, target, ...props }: AntdButtonProps) {
   const router = useRouter()
   return (
     <AntdButton
       {...props}
+      href={href}
+      target={target}
       onClick={event => {
         onClick?.(event)
         if (event.defaultPrevented) return
         if (!href || !href.startsWith('/')) return
+        if (target === '_blank') return
         event.preventDefault()
         router.push(href)
       }}
