8664aad77c67e8708f04106b313f4083de8f5845
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-03-31 10:54:23 +0200

    Add icons to success/failure highlighting

--- src/components/JournalMarkdown/journalMarkdownEmbellishmentRules.tsx
+++ src/components/JournalMarkdown/journalMarkdownEmbellishmentRules.tsx
@@ -250,13 +250,13 @@ function staticJournalEmbellishmentRules(
       'word:success',
       t('common.check_success_word'),
       true,
-      ({ slice, reactKey }) => accentSpan('green', slice, reactKey)
+      ({ slice, reactKey }) => accentSpan('green', `✓ ${slice}`, reactKey)
     ),
     journalLiteralRule(
       'word:failure',
       t('common.check_failure_word'),
       true,
-      ({ slice, reactKey }) => accentSpan('red', slice, reactKey)
+      ({ slice, reactKey }) => accentSpan('red', `✗ ${slice}`, reactKey)
     ),
     journalLiteralRule('symbol:sun', '☼', undefined, ({ slice, reactKey }) =>
       accentSpan('#d4a017', slice, reactKey)
