3baa0310bc5e1a410445b43465cfb0a503c1ba43
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-04-06 11:01:55 +0200

    refactor: restructure AppProviders to use ThemeProvider

--- src/components/AppProviders/AppProviders.tsx
+++ src/components/AppProviders/AppProviders.tsx
@@ -4,31 +4,18 @@ import { App, ConfigProvider } from 'antd'
 import antdLocale from 'antd/locale/fr_FR'
 import type { ReactNode } from 'react'
 import { NavigationBlockerProvider } from '@/components/AppProviders/NavigationBlockerContext'
+import { ThemeProvider } from '@/components/AppProviders/ThemeProvider'
 import { SettingsProvider } from '@/components/PageSettings/SettingsContext'
-
-const theme = {
-  token: {
-    colorPrimary: '#3d8b7a',
-    colorBgLayout: '#f6f9f7',
-    borderRadius: 10,
-    fontSize: 15,
-    fontFamily:
-      'var(--font-geist-sans), system-ui, -apple-system, BlinkMacSystemFont, sans-serif',
-  },
-  components: {
-    Layout: {
-      bodyBg: '#f6f9f7',
-    },
-  },
-}
 
 export function AppProviders({ children }: { children: ReactNode }) {
   return (
-    <ConfigProvider locale={antdLocale} theme={theme}>
+    <ConfigProvider locale={antdLocale}>
       <SettingsProvider>
-        <NavigationBlockerProvider>
-          <App>{children}</App>
-        </NavigationBlockerProvider>
+        <ThemeProvider>
+          <NavigationBlockerProvider>
+            <App>{children}</App>
+          </NavigationBlockerProvider>
+        </ThemeProvider>
       </SettingsProvider>
     </ConfigProvider>
   )
