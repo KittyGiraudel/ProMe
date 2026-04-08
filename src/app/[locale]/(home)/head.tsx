875047aa57f4dc14e1d76ab317efab1351cf9b7c
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-04-08 08:24:50 +0200

    Add prefetching to the landing page hero image

---
+++ src/app/[locale]/(home)/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <link
+        rel='preload'
+        as='image'
+        href='/images/home-cover.avif'
+        type='image/avif'
+        fetchPriority='high'
+      />
+    </>
+  )
+}
