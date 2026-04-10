d9d01feeaf78496aa6f759c27454d118dc882a0e
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-04-10 15:06:58 +0200

    Remove obsolete comments

--- src/app/serwist/[path]/route.ts
+++ src/app/serwist/[path]/route.ts
@@ -1,12 +1,6 @@
 import { spawnSync } from 'node:child_process'
 import { createSerwistRoute } from '@serwist/turbopack'
 
-// If you are using Next.js versions older than 15.0.0, add the
-// `nextConfig` option so that Serwist can configure the service
-// worker according to your options. Serwist 10 and newer will
-// only support Next.js 15.0.0 and above.
-// import nextConfig from "$cwd/next.config.mjs";
-
 // This is optional!
 // A revision helps Serwist version a precached page. This
 // avoids outdated precached responses being used. Using
@@ -21,7 +15,6 @@ export const { dynamic, dynamicParams, revalidate, generateStaticParams, GET } =
   createSerwistRoute({
     additionalPrecacheEntries: [{ url: '/~offline', revision }],
     swSrc: 'src/app/sw.ts',
-    // nextConfig,
     // If set to `false`, Serwist will attempt to use `esbuild-wasm`.
     useNativeEsbuild: true,
   })
