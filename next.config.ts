0b669effae0a64051ddba7e3df0d5269d3d94ac6
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-04-10 13:52:27 +0200

    Remove unnecessary configuration (default)

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  experimental: {
-    optimizePackageImports: ['antd'],
-  },
   async headers() {
     return [
       {
