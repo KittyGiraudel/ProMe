5f538237c1b566ccd7d0ef348d39d0af220cb067
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-04-10 13:31:06 +0200

    Fix preload the hero image not working

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -21,5 +21,16 @@ export default function Home({ params }: Props) {
 
   setRequestLocale(locale as AppConfig['Locale'])
 
-  return <LandingPage />
+  return (
+    <>
+      <link
+        rel='preload'
+        as='image'
+        href='/images/home-cover.avif'
+        type='image/avif'
+        fetchPriority='high'
+      />
+      <LandingPage />
+    </>
+  )
 }
