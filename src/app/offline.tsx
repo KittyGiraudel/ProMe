92043b2677e229a6055cb6cc1d260ac7bf05bbec
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-04-10 16:04:55 +0200

    Remove obsolete offline page

--- src/app/offline.tsx
+++
@@ -1,8 +0,0 @@
-export default function OfflinePage() {
-  return (
-    <div>
-      <h1>You're Offline</h1>
-      <p>This page is served from the cache. Please check your connection.</p>
-    </div>
-  )
-}
