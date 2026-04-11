ea89de30ba9c892aa7dfa69d0ece3adfd05796fb
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-04-11 13:19:52 +0200

    Add maskable icons to manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -18,6 +18,7 @@ export default function manifest(): MetadataRoute.Manifest {
           : '/web-app-manifest-192x192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'maskable any',
       },
       {
         src: isDev
@@ -25,6 +26,7 @@ export default function manifest(): MetadataRoute.Manifest {
           : '/web-app-manifest-512x512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'maskable any',
       },
     ],
   }
