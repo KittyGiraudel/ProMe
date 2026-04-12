37f7e874c1a97cf09d956b0bc947841f609b623c
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-04-12 10:38:12 +0200

    Add logout button to login page if logged in

--- src/components/PageLogin/PageLogin.tsx
+++ src/components/PageLogin/PageLogin.tsx
@@ -6,7 +6,7 @@ import { Layout } from '@/components/Layout/Layout'
 import { useAuth } from '@/lib/auth/context'
 
 export function PageLogin() {
-  const { oauthLogin, loading } = useAuth()
+  const { oauthLogin, loading, user, logout } = useAuth()
   const t = useTranslations()
 
   return (
@@ -19,11 +19,23 @@ export function PageLogin() {
       ]}>
       <Card
         title={t('auth.sign_in_with_google')}
-        actions={[
-          <Button onClick={() => oauthLogin()} loading={loading} type='primary'>
-            {t('auth.sign_in_with_google')}
-          </Button>,
-        ]}>
+        actions={
+          user
+            ? [
+                <Button key='logout' onClick={logout} loading={loading}>
+                  {t('nav.logout')}
+                </Button>,
+              ]
+            : [
+                <Button
+                  key='login'
+                  onClick={() => oauthLogin()}
+                  loading={loading}
+                  type='primary'>
+                  {t('auth.sign_in_with_google')}
+                </Button>,
+              ]
+        }>
         <Typography.Text>{t('auth.sign_in_prompt')}</Typography.Text>
       </Card>
     </Layout>
