83ef9b9178a6994bf6de201a7ed93eb2cbe764ce
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-04-15 14:52:00 +0200

    Add hexagon into the logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,9 @@
 import './Logo.css'
 
 export function Logo() {
-  return <span className='Logo'>ProMe</span>
+  return (
+    <span className='Logo'>
+      <span className='Logo__hex'>⬡</span> ProMe
+    </span>
+  )
 }
